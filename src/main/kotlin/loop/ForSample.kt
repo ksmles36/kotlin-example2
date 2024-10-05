be51c403a2eec35306e1fdc52f33df6199c35ab6
kimseongmin <ksmles36@gmail.com>
2024-10-05 11:35:56 +0900

    조건문, 반복문 실습1

---
+++ src/main/kotlin/loop/ForSample.kt
@@ -0,0 +1,12 @@
+package loop
+
+fun main(args: Array<String>) {
+    for (i in 1..5) println(i)
+    println()
+
+    for (i in 5 downTo 1) println(i)
+    println()
+
+    for (i in 1..5 step 2) println(i)
+    println()
+}
---
+++ src/main/kotlin/loop/WhileSample.kt
@@ -0,0 +1,30 @@
+package loop
+
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+    var a = Random.nextInt(10)
+
+    while (a > 0) {
+        if (a % 2 == 0) {
+            println("$a is even")
+            a--
+            continue
+        } else {
+            println(a)
+            a--
+        }
+    }
+
+    println()
+
+    var b = Random.nextInt(10)
+    do {
+        println(b)
+        if (b % 3 == 0) {
+            println("↑ break!!")
+            break
+        }
+        b--
+    } while (b > 0)
+}
