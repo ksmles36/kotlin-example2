5904f553d47037b6e0b1995c978b3674469921e7
kimseongmin <ksmles36@gmail.com>
2024-10-08 18:35:24 +0900

    반복문 실습3

--- src/main/kotlin/loop/ForSample.kt
+++ src/main/kotlin/loop/ForSample.kt
@@ -9,4 +9,10 @@ fun main(args: Array<String>) {
 
     for (i in 1..5 step 2) println(i)
     println()
+
+    val num1: Int = 2
+    val num2: Int = 4
+    for (n in num1..num2) println(n)
+    
+
 }
